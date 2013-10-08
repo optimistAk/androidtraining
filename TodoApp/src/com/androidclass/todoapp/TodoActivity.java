610d2f0dd10a6ce9c7cb510c6a884b7e9ca40340
Aravind K <aravind_iiit@yahoo.com>
2013-10-07 22:51:29 -0700

    Todo List app: just trying out, with basic add item onclick

---
+++ TodoApp/src/com/androidclass/todoapp/TodoActivity.java
@@ -0,0 +1,56 @@
+package com.androidclass.todoapp;
+
+import java.util.ArrayList;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class TodoActivity extends Activity {
+
+	// Variables in global context for this app
+	
+	private ArrayList<String> itemList; 
+	private ArrayAdapter<String> todoAdapter;
+	private ListView lvItems;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_todo);
+		
+		populateArraylistItems();
+		lvItems = (ListView) findViewById(R.id.lvItems);
+
+		todoAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemList);
+		lvItems.setAdapter(todoAdapter);
+		itemList.add("get milk");
+	}
+
+	private void populateArraylistItems(){
+		itemList = new ArrayList<String>();
+		itemList.add("wake up");
+		itemList.add("turnoff alarm");
+		itemList.add("brush teeth");
+	}
+	
+	public void onClickAddTodoItem(View v){
+		EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+		this.todoAdapter.add(etNewItem.getText().toString());
+		etNewItem.setText("");
+		Toast.makeText(this, "Item added", Toast.LENGTH_LONG).show();		
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.todo, menu);
+		return true;
+	}
+
+}
