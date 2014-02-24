b695084667a30db9a213315b78a5cbca3c4b3428
Aravind K <aravind_iiit@yahoo.com>
2014-02-24 00:19:25 -0800

    Adding some code to extend the TodoApp

---
+++ TodoApp/src/com/androidclass/todoapp/CommonLib.java
@@ -0,0 +1,18 @@
+package com.androidclass.todoapp;
+
+import org.json.JSONObject;
+
+
+public class CommonLib {
+
+	public static String dailyHoroscope(){
+		JSONObject jObj = new JSONObject();
+		return "Today you will rock dude!!";
+
+	}
+	
+	public static String colorToWear(){
+		
+		return "blue";
+	}
+}
--- TodoApp/src/com/androidclass/todoapp/TodoActivity.java
+++ TodoApp/src/com/androidclass/todoapp/TodoActivity.java
@@ -3,9 +3,18 @@ package com.androidclass.todoapp;
 import java.util.ArrayList;
 
 import com.androidclass.groceryassistapp.R;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -31,14 +40,48 @@ public class TodoActivity extends Activity {
 
 		todoAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemList);
 		lvItems.setAdapter(todoAdapter);
-		itemList.add("organic 2% milk");
+		itemList.add("Daily Horoscope" + CommonLib.dailyHoroscope());
+		
+		AsyncHttpClient client = new AsyncHttpClient();
+		
+//		client.get("http://pipes.yahoo.com/pipes/pipe.run?_id=4c472300378d711f542007004321a950&_render=json", new AsyncHttpResponseHandler() {
+//		    @Override
+//		    public void onSuccess(String response) {
+//		        System.out.println("AKKKKK" + response);
+//		        Log.d("DEBUG", "AKKK" + response.toString());
+//		    }
+//		});
+		
+		client.get("http://pipes.yahoo.com/pipes/pipe.run?_id=4c472300378d711f542007004321a950&_render=json", new JsonHttpResponseHandler() {
+			@Override
+			public void onSuccess(JSONObject response) {	
+				Log.d("DEBUG", "AKK");
+				JSONArray horoscopeJsonResults = null;
+				try {
+					Log.d("DEBUG", "AKK");
+					//Log.d("DEBUG", response.getJSONObject("value").getJSONArray("items").toString());
+					
+					horoscopeJsonResults = response.getJSONObject("value").getJSONArray("items");
+					
+					Log.d("DEBUG", horoscopeJsonResults.getJSONObject(0).getString("description").toString());
+					
+					itemList.add("Aravind is good");
+					//itemList.add(horoscopeJsonResults.getJSONObject(0).getString("description").toString());
+					
+					
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 	private void populateArraylistItems(){
 		itemList = new ArrayList<String>();
-		itemList.add("AA eggs");
-		itemList.add("Yoplait youghurt");
-		itemList.add("Onions");
+		itemList.add("Color to wear today:" + CommonLib.colorToWear());
+		itemList.add("Weather: cloudy, 77F, humid in the evening");
+//		itemList.add("Yoplait youghurt");
+//		itemList.add("Onions");
 	}
 	
 	public void onClickAddTodoItem(View v){
