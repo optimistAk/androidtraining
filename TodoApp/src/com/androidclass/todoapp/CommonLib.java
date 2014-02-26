393a010e28b3407892bedb23559f2022a3370ec4
Aravind K <aravind_iiit@yahoo.com>
2014-02-26 00:26:52 -0800

    Adding zodiac fetch based on birthday, and horoscope fetch... and some clean up

--- TodoApp/src/com/androidclass/todoapp/CommonLib.java
+++ TodoApp/src/com/androidclass/todoapp/CommonLib.java
@@ -24,6 +24,40 @@ public class CommonLib {
 	
 	 *
 	 */
+	
+	public static String findZodiacSign (String month, String day) {
+        int M = Integer.parseInt(month);
+        int D = Integer.parseInt(day);
+        if ((M == 12 && D >= 22 && D <= 31) || (M ==  1 && D >= 1 && D <= 19))
+            return "Capricorn";
+        else if ((M ==  1 && D >= 20 && D <= 31) || (M ==  2 && D >= 1 && D <= 17))
+            return "Aquarius";
+        else if ((M ==  2 && D >= 18 && D <= 29) || (M ==  3 && D >= 1 && D <= 19))
+            return "Pisces";
+        else if ((M ==  3 && D >= 20 && D <= 31) || (M ==  4 && D >= 1 && D <= 19))
+            return "Aries";
+        else if ((M ==  4 && D >= 20 && D <= 30) || (M ==  5 && D >= 1 && D <= 20))
+            return "Taurus";
+        else if ((M ==  5 && D >= 21 && D <= 31) || (M ==  6 && D >= 1 && D <= 20))
+            return "Gemini";
+        else if ((M ==  6 && D >= 21 && D <= 30) || (M ==  7 && D >= 1 && D <= 22))
+            return "Cancer";
+        else if ((M ==  7 && D >= 23 && D <= 31) || (M ==  8 && D >= 1 && D <= 22))
+            return "Leo";
+        else if ((M ==  8 && D >= 23 && D <= 31) || (M ==  9 && D >= 1 && D <= 22))
+            return "Virgo";
+        else if ((M ==  9 && D >= 23 && D <= 30) || (M == 10 && D >= 1 && D <= 22))
+            return "Libra";
+        else if ((M == 10 && D >= 23 && D <= 31) || (M == 11 && D >= 1 && D <= 21))
+            return "Scorpio";
+        else if ((M == 11 && D >= 22 && D <= 30) || (M == 12 && D >= 1 && D <= 21))
+            return "Sagittarius";
+        else
+            return "Illegal date";
+
+    }
+
+	
 	public static String colorToWear(int temperature, boolean isCloudy){
 		
 		String dressColor = "blue";
--- TodoApp/src/com/androidclass/todoapp/TodoActivity.java
+++ TodoApp/src/com/androidclass/todoapp/TodoActivity.java
@@ -36,168 +36,166 @@ import android.widget.Toast;
 public class TodoActivity extends Activity {
 
 	// Variables in global context for this app
-	
+
 	private ArrayList<String> itemList; 
 	private ArrayAdapter<String> todoAdapter;
 	private ListView lvItems;
 	public String horoscopeText="hello";
-	
-	  private LocationManager locationManager;
-	  private String provider;
-	  private MyLocationListener mylistener;
-	  private Criteria criteria;
-	
+
+	private LocationManager locationManager;
+	private String provider;
+	private MyLocationListener mylistener;
+	private Criteria criteria;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_todo);
-		
+
 		populateArraylistItems();
 		lvItems = (ListView) findViewById(R.id.lvItems);
 
 		todoAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemList);
 		lvItems.setAdapter(todoAdapter);
-		itemList.add("Daily Horoscope: " + CommonLib.dailyHoroscope());
-		
+
 		AsyncHttpClient client = new AsyncHttpClient();
-		
+
 		client.get("http://pipes.yahoo.com/pipes/pipe.run?_id=4c472300378d711f542007004321a950&_render=json", new JsonHttpResponseHandler() {
 			@Override
 			public void onSuccess(JSONObject response) {	
 				JSONArray horoscopeJsonResults = null;
 				try {				
 					horoscopeJsonResults = response.getJSONObject("value").getJSONArray("items");				
-					
+
 					// Pass the index value based on what sunsign, here, 0->Aries, 1->Taurus ... etc				
 					String horoscopeFullString = horoscopeJsonResults.getJSONObject(0).getString("description").toString();
-					
+
 					Document doc = Jsoup.parse(horoscopeFullString);
 					Element p= doc.select("p").first();
 					horoscopeText = p.text();
 
-					Log.d("DEBUG", horoscopeText);
-					
-					todoAdapter.add("Daily Horoscope" + horoscopeText);
-					
+					// Replace this with birth date
+					todoAdapter.add("Daily Horoscope for " + CommonLib.findZodiacSign("9", "6") + ": \n" + horoscopeText);
+
 				} catch (JSONException e) {
 					e.printStackTrace();
 				}
 			}
 		});
-		
-		
-		
-//		client.get("http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&APPID=055a946d7193b12bb5ad491595be1124", new JsonHttpResponseHandler() {
-//			@Override
-//			public void onSuccess(JSONObject response) {	
-//				try {
-//					Log.d("DEBUG", response.getJSONObject("main").getString("temp").toString());
-//				} catch (JSONException e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-		
-		 // Get the location manager
-		  locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		  // Define the criteria how to select the location provider
-		  criteria = new Criteria();
-		  criteria.setAccuracy(Criteria.ACCURACY_COARSE);	//default
-		  
 
-		  criteria.setCostAllowed(false); 
-		  // get the best provider depending on the criteria
-		  provider = locationManager.getBestProvider(criteria, false);
-	    
-		  // the last known location of this provider
-		  Location location = locationManager.getLastKnownLocation(provider);
 
-		  mylistener = new MyLocationListener();
-	
-		  if (location != null) {
-			  mylistener.onLocationChanged(location);
-		  } else {
-			  // leads to the settings because there is no last known location
-			  Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-			  startActivity(intent);
-		  }
-		  // location updates: at least 1 meter and 200millsecs change
-		  //NETWORK_PROVIDER
-		  locationManager.requestLocationUpdates(provider, 200, 1, mylistener);
-		
-		
+
+		//		client.get("http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&APPID=055a946d7193b12bb5ad491595be1124", new JsonHttpResponseHandler() {
+		//			@Override
+		//			public void onSuccess(JSONObject response) {	
+		//				try {
+		//					Log.d("DEBUG", response.getJSONObject("main").getString("temp").toString());
+		//				} catch (JSONException e) {
+		//					e.printStackTrace();
+		//				}
+		//			}
+		//		});
+
+		// Get the location manager
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		// Define the criteria how to select the location provider
+		criteria = new Criteria();
+		criteria.setAccuracy(Criteria.ACCURACY_COARSE);	//default
+
+
+		criteria.setCostAllowed(false); 
+		// get the best provider depending on the criteria
+		provider = locationManager.getBestProvider(criteria, false);
+
+		// the last known location of this provider
+		Location location = locationManager.getLastKnownLocation(provider);
+
+		mylistener = new MyLocationListener();
+
+		if (location != null) {
+			mylistener.onLocationChanged(location);
+		} else {
+			// leads to the settings because there is no last known location
+			Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+			startActivity(intent);
+		}
+		// location updates: at least 1 meter and 200millsecs change
+		//NETWORK_PROVIDER
+		locationManager.requestLocationUpdates(provider, 200, 1, mylistener);
+
+
 	}
 
 	private void populateArraylistItems(){
 		itemList = new ArrayList<String>();
 		itemList.add("Color to wear today: " + CommonLib.colorToWear(64, false));
 		itemList.add("Weather: cloudy, 77F, humid in the evening");
-//		itemList.add("Yoplait youghurt");
-//		itemList.add("Onions");
+		//		itemList.add("Yoplait youghurt");
+		//		itemList.add("Onions");
 	}
-	
+
 	public void onClickAddTodoItem(View v){
 		//EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
 		//this.todoAdapter.add(etNewItem.getText().toString());
 		//etNewItem.setText("");
 		Toast.makeText(this, "Item added", Toast.LENGTH_LONG).show();		
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.todo, menu);
 		return true;
 	}
-	
-	 private class MyLocationListener implements LocationListener {
-			
-		  @Override
-		  public void onLocationChanged(Location location) {
+
+	private class MyLocationListener implements LocationListener {
+
+		@Override
+		public void onLocationChanged(Location location) {
 			// Initialize the location fields
-			  
-				AsyncHttpClient client_w = new AsyncHttpClient();
 
-				client_w.get("http://api.openweathermap.org/data/2.5/weather?lat=" + location.getLatitude() + "&lon=" + location.getLongitude() + "&APPID=055a946d7193b12bb5ad491595be1124", new JsonHttpResponseHandler() {
-	                 @Override
-					public void onSuccess(JSONObject response) {
-	                	 Log.d("DEBUG", "weather0");
-						//String weatherResult = null;
-						try{
-							String weatherResult = response.getJSONObject("main").getString("temp").toString();
-							String cloudResult = response.getJSONObject("clouds").getString("all").toString();
-							String cdescResult = response.getJSONArray("weather").getJSONObject(0).getString("description");
-							Log.d("DEBUG", weatherResult);
-							Log.d("DEBUG", cloudResult);
-							Log.d("DEBUG", cdescResult);
-							
-						} catch (JSONException e) {
-							Log.d("DEBUG", "weather2");
-							e.printStackTrace();
-						}
+			AsyncHttpClient client_w = new AsyncHttpClient();
+
+			client_w.get("http://api.openweathermap.org/data/2.5/weather?lat=" + location.getLatitude() + "&lon=" + location.getLongitude() + "&APPID=055a946d7193b12bb5ad491595be1124", new JsonHttpResponseHandler() {
+				@Override
+				public void onSuccess(JSONObject response) {
+					Log.d("DEBUG", "weather0");
+					//String weatherResult = null;
+					try{
+						String weatherResult = response.getJSONObject("main").getString("temp").toString();
+						String cloudResult = response.getJSONObject("clouds").getString("all").toString();
+						String cdescResult = response.getJSONArray("weather").getJSONObject(0).getString("description");
+						Log.d("DEBUG", weatherResult);
+						Log.d("DEBUG", cloudResult);
+						Log.d("DEBUG", cdescResult);
+
+					} catch (JSONException e) {
+						Log.d("DEBUG", "weather2");
+						e.printStackTrace();
 					}
-				  
-				});
-		  }
-	
-		  @Override
-		  public void onStatusChanged(String provider, int status, Bundle extras) {
-			  Toast.makeText(TodoActivity.this, provider + "'s status changed to "+status +"!",
-				        Toast.LENGTH_SHORT).show();
-		  }
-	
-		  @Override
-		  public void onProviderEnabled(String provider) {
-			  Toast.makeText(TodoActivity.this, "Provider " + provider + " enabled!",
-		        Toast.LENGTH_SHORT).show();
-	
-		  }
-	
-		  @Override
-		  public void onProviderDisabled(String provider) {
-			  Toast.makeText(TodoActivity.this, "Provider " + provider + " disabled!",
-		        Toast.LENGTH_SHORT).show();
-		  }
+				}
 
-     }
+			});
+		}
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			Toast.makeText(TodoActivity.this, provider + "'s status changed to "+status +"!",
+					Toast.LENGTH_SHORT).show();
+		}
+
+		@Override
+		public void onProviderEnabled(String provider) {
+			Toast.makeText(TodoActivity.this, "Provider " + provider + " enabled!",
+					Toast.LENGTH_SHORT).show();
+
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) {
+			Toast.makeText(TodoActivity.this, "Provider " + provider + " disabled!",
+					Toast.LENGTH_SHORT).show();
+		}
+
+	}
 }
